c3a0491f7163cf344e9bf7accda5b4c3cd2ca118
Dhanabordee Mekintharanggur <53035529+pointmekin@users.noreply.github.com>
2023-01-14 22:06:43 +0700

    wip: update general UI (#3)
    Co-authored-by: Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>

--- components/UI/navigation/Footer.tsx
+++ components/UI/navigation/Footer.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, makeStyles, Typography } from '@mui/material'
+import Link from 'next/link';
 
 function Footer() {
   return (
-    <Box sx={{
-      backgroundColor: 'white',
-      padding: 0,
-      mx: 0,
-      mt: 4,
-      p: 6
-    }}>
-      <Typography>WARDEN 2022</Typography>
-    </Box>
+    <footer>
+      <Box sx={{ padding: '1rem 0 1rem 0', marginTop: '20px', backgroundColor: 'white', width: '100%' }}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Warden
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" marginBottom={2} component="p">
+          Platform for Combatting Event Ticketing Exploits using NFT Utility Platform with Added Revenue Generation Capabilities 
+        </Typography>
+        <Copyright />
+      </Box>
+    </footer>
   )
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://google.com/">
+        Warden
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
 export default Footer
