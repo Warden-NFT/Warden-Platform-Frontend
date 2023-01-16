eba2d858ee076df9baa7a8822c715c0e7ad0d618
ChuChuwiiii <chuwong35122@gmail.com>
2023-01-16 21:29:33 +0700

    feat: add customizable props to the component

--- components/generate/GenerateModeSelector.tsx
+++ components/generate/GenerateModeSelector.tsx
@@ -26,10 +26,11 @@ function GenerateModeSelector({ selectedType }: Props) {
             }}
           >
             <ImageLabelCard
-              backgroundColor="white"
               imageSrc="/images/generate/event-scheduling.jpg"
               imageAlt="Create event completely"
-              width="100%"
+              title="Create with Complete Assets"
+              description="Ticket will be created with your uploaded assets such as image or video. You can also customize the quantity of each assets to be distributed to your customers."
+              containerStyles={{ backgroundColor: 'white', width: '100%' }}
             />
           </div>
 
@@ -44,10 +45,11 @@ function GenerateModeSelector({ selectedType }: Props) {
             }}
           >
             <ImageLabelCard
-              backgroundColor="white"
               imageSrc="/images/generate/complete-asset-card.avif"
               imageAlt="Create event completely"
-              width="100%"
+              title="Create with Layered Assets"
+              description="Ticket will be created with your uploaded assets and randomly generate by its rarity. You can customize the rarity of each asset or layer."
+              containerStyles={{ backgroundColor: 'white', width: '100%' }}
             />
           </div>
         </Stack>
