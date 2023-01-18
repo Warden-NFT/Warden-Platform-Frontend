7295ef03de0b8e3e8f7573cc50b752da44a2fa7c
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-01-18 22:58:43 +0700

    refactor: env type declaration

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -1,15 +1,14 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      ALCHEMY_ID: string;
-
-      NODE_ENV: "development" | "production";
-      PORT?: string;
-      PWD: string;
+      NEXT_PUBLIC_WARDEN_API_URL: string
+      NEXT_PUBLIC_APP_API_URL: string
+      NEXT_PUBLIC_SENTRY_DSN: string
+      NEXT_PUBLIC_ALCHEMY_KEY: string
     }
   }
 }
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {};
+export {}
