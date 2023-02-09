32459a8bb5729318e3a96637de3d8ead89d0c99c
ChuChuwiiii <chuwong35122@gmail.com>
2023-02-09 10:37:45 +0700

    feat: create input for price and currency select

---
+++ components/UI/input/ControlledCurrencyPriceSelect.tsx
@@ -0,0 +1,92 @@
+import {
+  FormControl,
+  FormLabel,
+  MenuItem,
+  TextField,
+  Typography
+} from "@mui/material"
+import React from "react"
+import { SUPPORTED_DIGITAL_CURRENCIES } from "../../../constants/currencies/digital"
+
+interface Props {
+  label: string
+  amountName: string
+  amountValue: number
+  handleChange: any
+  amountError?: boolean
+  amountTouched?: boolean
+  currencyName?: string
+  currencyValue: string
+  currencyError?: boolean
+  currencyTouched?: boolean
+  selectDisabled?: boolean
+}
+
+function ControlledCurrencyPriceSelect({
+  label,
+  amountName,
+  amountValue,
+  amountTouched,
+  amountError,
+  handleChange,
+  currencyName,
+  currencyValue,
+  currencyError,
+  currencyTouched,
+  selectDisabled
+}: Props) {
+  return (
+    <FormControl required>
+      <FormLabel>{label}</FormLabel>
+      <Typography variant="caption" color="gray">
+        {label}
+      </Typography>
+      <TextField
+        name={amountName}
+        value={amountValue}
+        onChange={handleChange}
+        id={`${amountName}-input`}
+        data-testid={`${amountName}-input`}
+        type="number"
+        variant="outlined"
+        size="small"
+        error={amountError != null}
+        helperText={amountTouched && amountError ? amountError : undefined}
+        InputProps={{
+          endAdornment: (
+            <TextField
+              disabled={selectDisabled}
+              variant="filled"
+              size="small"
+              id={`${currencyName}-input`}
+              select
+              value={currencyValue}
+              onChange={handleChange}
+              error={currencyError != null}
+              helperText={
+                currencyTouched && currencyError ? currencyError : undefined
+              }
+              sx={{
+                width: "300px",
+                backgroundColor: "white",
+                marginLeft: 4,
+                marginRight: 0,
+                "&.Mui-disabled": {
+                  color: "orange"
+                }
+              }}
+            >
+              {SUPPORTED_DIGITAL_CURRENCIES.map((currency, key) => (
+                <MenuItem key={key} value={currency.symbol}>
+                  {currency.name}
+                </MenuItem>
+              ))}
+            </TextField>
+          )
+        }}
+      />
+    </FormControl>
+  )
+}
+
+export default ControlledCurrencyPriceSelect
--- components/generate/complete/steps/CreateCompleteTicketStep4.tsx
+++ components/generate/complete/steps/CreateCompleteTicketStep4.tsx
@@ -1,4 +1,15 @@
-import { Stack, Typography } from "@mui/material"
+import {
+  Box,
+  CircularProgress,
+  FormControl,
+  FormLabel,
+  MenuItem,
+  Stack,
+  TextField,
+  Typography
+} from "@mui/material"
+import { purple } from "@mui/material/colors"
+import { useFormik } from "formik"
 import Image from "next/image"
 import React, { useContext, useEffect, useState } from "react"
 import { GenerateCompleteContext } from "../../../../contexts/generate/GenerateCompleteContext"
@@ -6,6 +17,7 @@ import { LayoutContext } from "../../../../contexts/layout/LayoutContext"
 import { useStorageBucket } from "../../../../hooks/useStorageBucket"
 import { StoredAsset } from "../../../../interfaces/gcp/storage.interface"
 import FlatCard from "../../../UI/card/FlatCard"
+import ControlledCurrencyPriceSelect from "../../../UI/input/ControlledCurrencyPriceSelect"
 import ControlledStepperButtons from "../../../UI/navigation/ControlledStepperButtons"
 
 function CreateCompleteTicketStep4() {
@@ -20,6 +32,19 @@ function CreateCompleteTicketStep4() {
   const { saveFile } = useStorageBucket()
   const { setShowLoadingBackdrop } = useContext(LayoutContext)
   const [uploaded, setUploaded] = useState(false)
+
+  const { values, handleChange, touched, errors, handleSubmit } = useFormik({
+    initialValues: {
+      allowResale: true,
+      resaleCeilingPrice: 0,
+      resaleFloorPrice: 0,
+      enableRoyaltyFee: true,
+      royaltyFeePercentage: 5
+    },
+    onSubmit: (data) => {
+      console.log(data)
+    }
+  })
 
   useEffect(() => {
     setShowLoadingBackdrop(true)
@@ -63,19 +88,43 @@ function CreateCompleteTicketStep4() {
 
   return (
     <FlatCard>
-      <Typography variant="h3" component="h1">
-        Create Ticket For You
-      </Typography>
-      <Typography>Hang tight! your ticket is being create...</Typography>
-      <Stack direction="column" sx={{ placeItems: "center" }}>
-        <Image
-          src="/gifs/generate/nft-minting.gif"
-          width="400"
-          height="400"
-          alt="Minting"
-        />
-        <Typography fontWeight="500">Generating</Typography>
+      <Box sx={{ marginBottom: 2 }}>
+        <Typography variant="h3" component="h1">
+          Customize NFTs Utility
+        </Typography>
+      </Box>
+
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ padding: 2, backgroundColor: purple[50], borderRadius: 2 }}
+      >
+        <Box>
+          <Typography variant="body1" component="h3" fontWeight="600">
+            Uploading your assets
+          </Typography>
+          <Typography variant="subtitle1">
+            Hang tight! Your files are uploading...
+          </Typography>
+        </Box>
+        <CircularProgress />
       </Stack>
+
+      <Stack spacing={2} sx={{ marginTop: 4 }}>
+        <Typography variant="h6">Resellings</Typography>
+        <ControlledCurrencyPriceSelect
+          label="Select minimum resale price"
+          amountName="resaleFloorPrice"
+          amountValue={values.resaleFloorPrice}
+          handleChange={handleChange}
+          amountError={Boolean(errors.resaleCeilingPrice)}
+          amountTouched={touched.resaleFloorPrice}
+          currencyValue={formInfo.currency}
+          selectDisabled
+        />
+      </Stack>
+
       <ControlledStepperButtons
         handlePrevious={() => setActiveStep((prev) => prev - 1)}
         handleNext={() => setActiveStep((prev) => prev + 1)}
