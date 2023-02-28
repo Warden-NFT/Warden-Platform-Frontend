bbefcfebb7d689d107a345adf6f5c8e0bb75fc5a
ChuChuwiiii <chuwong35122@gmail.com>
2023-02-28 15:28:27 +0700

    feat: ticket info UI

--- pages/marketplace/[organizerId]/[eventId]/[ticketId]/index.tsx
+++ pages/marketplace/[organizerId]/[eventId]/[ticketId]/index.tsx
@@ -1,22 +1,31 @@
-import { Box, IconButton, Stack, Typography } from "@mui/material"
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+  Typography
+} from "@mui/material"
 import { useRouter } from "next/router"
 import React from "react"
 import EventInfoBanner from "../../../../../components/market/event/EventInfoBanner"
 import BannerLayout from "../../../../../components/UI/layout/BannerLayout"
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos"
-import { GetServerSideProps, NextPage } from "next"
+import { GetServerSideProps } from "next"
 import { EventTicket } from "../../../../../dtos/ticket/ticket.dto"
 import TicketCard from "../../../../../components/market/ticket/TicketCard"
 import axios, { AxiosError } from "axios"
 import { Event } from "../../../../../interfaces/event/event.interface"
 import { EventOrganizerUser } from "../../../../../interfaces/auth/user.interface"
 import { MarketTickets } from "../../../../../interfaces/market/marketEvent.interface"
+import { blue, grey } from "@mui/material/colors"
+import moment from "moment"
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const eventId = params?.eventId
   const ticketId = params?.ticketId
 
-  // todo: add event org and fetch
   try {
     const marketTicketRes = await axios.get<MarketTickets>(
       `${process.env.NEXT_PUBLIC_WARDEN_API_URL}/market/tickets`,
@@ -32,7 +41,6 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       ...(ticketCollection.tickets.general ?? []),
       ...(ticketCollection.tickets.vip ?? [])
     ]
-    console.log({ tickets })
     const ticket = tickets.find((ticket) => ticket._id === ticketId)
     if (!ticket) {
       throw Error("No ticket found")
@@ -47,7 +55,6 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     }
   } catch (e) {
     const err = e as AxiosError
-    console.log(err)
     return {
       notFound: true
     }
@@ -62,9 +69,20 @@ interface PageProps {
 
 const MarketTicket = ({ ticket, event, organizer }: PageProps) => {
   const router = useRouter()
+  const isSold = Math.random() > 0.5
+
+  function getEventLocationUrl() {
+    if (event.online_url) {
+      return event.online_url
+    } else if (event.location?.place_id) {
+      return `https://www.google.com/maps/place/?q=place_id:${event.location.place_id}`
+    }
+
+    return "/marketplace"
+  }
 
   return (
-    <div>
+    <Container>
       <BannerLayout
         backgroundImage={event.image as string}
         title=""
@@ -92,17 +110,143 @@ const MarketTicket = ({ ticket, event, organizer }: PageProps) => {
           </IconButton>
           <Typography>Back to all tickets</Typography>
         </Stack>
-        <Box sx={{ border: 2, borderRadius: 2, padding: 4 }}>
-          <TicketCard
-            ticketId={ticket?._id ?? ""}
-            image={ticket?.ticketMetadata[0].image as string}
-            name={ticket?.name ?? ""}
-            ticketTypeLabel={ticket?.ticketType ?? ""}
-            price={(ticket?.price?.amount ?? "").toString()}
-          />
+        <Box
+          sx={{
+            border: 2,
+            borderRadius: 2,
+            padding: 2,
+            backgroundColor: "white"
+          }}
+        >
+          <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+            <Box sx={{ position: "relative" }}>
+              <TicketCard
+                ticketId={ticket?._id ?? ""}
+                image={ticket?.ticketMetadata[0].image as string}
+                name={ticket?.name ?? ""}
+                ticketTypeLabel={ticket?.ticketType ?? ""}
+                price={(ticket?.price?.amount ?? "").toString()}
+                sx={
+                  isSold ? { filter: "saturate(0.5)", opacity: 0.7 } : undefined
+                }
+              />
+              {isSold && (
+                <Typography
+                  variant="h3"
+                  fontWeight="600"
+                  color={grey[100]}
+                  sx={{ position: "absolute", top: "80px", left: "40px" }}
+                >
+                  SOLD
+                </Typography>
+              )}
+            </Box>
+            <Box sx={{ width: "100%", marginLeft: 4 }}>
+              <Alert
+                severity="info"
+                sx={{
+                  backgroundColor: blue[500],
+                  color: "white",
+                  marginBottom: 2
+                }}
+              >
+                This is a resale ticket by #addressId
+              </Alert>
+              <Stack spacing={2}>
+                <Typography variant="h6" component="h1">
+                  Ticket Details
+                </Typography>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography color={grey[500]}>Ticket Name</Typography>
+                  <Typography>{ticket.name}</Typography>
+                </Stack>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography color={grey[500]}>Description</Typography>
+                  <Typography>{ticket.description}</Typography>
+                </Stack>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography color={grey[500]}>Type</Typography>
+                  <Typography>{ticket.ticketType}</Typography>
+                </Stack>
+                <Typography variant="h6" component="h1">
+                  Event Details
+                </Typography>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography color={grey[500]}>Event Date</Typography>
+                  <Typography>
+                    {moment(event.startDate).format("lll")}
+                  </Typography>
+                </Stack>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography color={grey[500]}>Door Time</Typography>
+                  <Typography>
+                    {moment(event.doorTime).format("lll")}
+                  </Typography>
+                </Stack>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography color={grey[500]}>Location</Typography>
+                  <a
+                    target="_blank"
+                    href={getEventLocationUrl()}
+                    rel="noopener noreferrer"
+                  >
+                    <Typography>
+                      {event.location?.structured_formatting.main_text ||
+                        event.online_url}
+                    </Typography>
+                  </a>
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
         </Box>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{
+            marginY: 4,
+            border: 2,
+            borderRadius: 2,
+            padding: 2,
+            backgroundColor: "white"
+          }}
+        >
+          <Stack alignItems="start">
+            <Typography>Want to claim this ticket? Buy now</Typography>
+            <Typography fontWeight="700">
+              {ticket.price.amount} {ticket.price.currency}
+            </Typography>
+          </Stack>
+          <Button variant="contained">
+            <Typography>Purchase Ticket</Typography>
+          </Button>
+        </Stack>
       </BannerLayout>
-    </div>
+    </Container>
   )
 }
 
