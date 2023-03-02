b7084b1d736f9f84e84c07907c20001d96d861f6
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-02 11:36:05 +0700

    feat: add header to ticket page

--- pages/ticket/index.tsx
+++ pages/ticket/index.tsx
@@ -2,12 +2,16 @@ import { Container, Box, Typography } from "@mui/material"
 import React from "react"
 import GenerateModeSelector from "../../components/generate/GenerateModeSelector"
 import { useAuthAccount } from "../../hooks/useAuthAccount"
+import Head from "next/head"
 
 function GeneratePage() {
   useAuthAccount()
 
   return (
     <Container sx={{ display: "grid", placeItems: "center", paddingTop: 4 }}>
+      <Head>
+        <title>Create a Ticket</title>
+      </Head>
       <Typography variant="h4" fontWeight="800">
         Welcome to NFT ticket generator
       </Typography>
