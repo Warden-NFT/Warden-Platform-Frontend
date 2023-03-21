c3326bb5595b07946c0ef9da208c6e1e45983225
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-03-21 15:07:54 +0700

    feat: fix event search bug

--- components/market/search/EventSearchBar.tsx
+++ components/market/search/EventSearchBar.tsx
@@ -1,6 +1,12 @@
 import { LocationOnOutlined, Search } from "@mui/icons-material"
 import { DatePicker } from "@mui/x-date-pickers"
-import { FormControl, InputAdornment, TextField } from "@mui/material"
+import {
+  Box,
+  FormControl,
+  Grid,
+  InputAdornment,
+  TextField
+} from "@mui/material"
 import { useFormik } from "formik"
 import moment from "moment"
 import React, { useContext } from "react"
@@ -35,107 +41,138 @@ function EventSearchBar() {
             : undefined,
           searchTerm: data.searchTerm || undefined
         }
+        if (
+          !data.location &&
+          !data.startDate &&
+          !data.endDate &&
+          !data.searchTerm
+        ) {
+          return
+        }
         await searchEvents(payload)
       }
     })
 
   return (
     <form>
-      <ContainerCard sx={{ height: 40, display: "flex", gap: 2 }}>
-        <TextFieldWrapper
-          name="location"
-          label="Location"
-          value={values.location}
-          onChange={handleChange}
-          id="location-input"
-          data-testid="location-input"
-          placeholder="Location"
-          variant="outlined"
-          size="small"
-          multiline
-          maxRows={3}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <LocationOnOutlined />
-              </InputAdornment>
-            )
-          }}
-          error={errors.location != null}
-          helperText={touched.location ? errors.location : undefined}
-        />
-
-        <FormControl required sx={{ width: "100%", height: 84, maxWidth: 180 }}>
-          <DatePicker
-            renderInput={(props) => (
-              <TextField
-                name="startDate"
-                size="small"
-                variant="outlined"
-                {...props}
-                error={Boolean(errors.startDate)}
-                helperText={errors.startDate}
+      <ContainerCard sx={{ display: "flex" }}>
+        <Grid container gap={0} spacing={2}>
+          <Grid item xs={6} lg={4} sx={{ height: 40 }}>
+            <TextFieldWrapper
+              name="location"
+              label="Location"
+              value={values.location}
+              onChange={handleChange}
+              id="location-input"
+              data-testid="location-input"
+              placeholder="Location"
+              variant="outlined"
+              size="small"
+              multiline
+              maxRows={3}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <LocationOnOutlined />
+                  </InputAdornment>
+                )
+              }}
+              error={errors.location != null}
+              helperText={touched.location ? errors.location : undefined}
+              sx={{ width: "100%" }}
+            />
+          </Grid>
+          <Grid item xs={6} lg={4} sx={{ height: 40 }}>
+            <FormControl required sx={{ width: "100%", height: 84 }}>
+              <DatePicker
+                renderInput={(props) => (
+                  <TextField
+                    name="startDate"
+                    size="small"
+                    variant="outlined"
+                    {...props}
+                    error={Boolean(errors.startDate)}
+                    helperText={errors.startDate}
+                    sx={{ width: "100% !important" }}
+                  />
+                )}
+                label="Event start date"
+                value={values.startDate}
+                onChange={(newValue) => {
+                  if (!newValue) setFieldValue("endDate", null)
+                  setFieldValue(
+                    "startDate",
+                    moment(newValue, "DD/MM/YYYY HH:mm:ss").toDate()
+                  )
+                }}
               />
-            )}
-            label="Event start date"
-            value={values.startDate}
-            onChange={(newValue) => {
-              if (!newValue) setFieldValue("endDate", null)
-              setFieldValue(
-                "startDate",
-                moment(newValue, "DD/MM/YYYY HH:mm:ss").toDate()
-              )
-            }}
-          />
-        </FormControl>
-
-        <FormControl required sx={{ width: "100%", height: 84, maxWidth: 180 }}>
-          <DatePicker
-            renderInput={(props) => (
-              <TextField
-                name="endDate"
-                size="small"
-                variant="outlined"
-                {...props}
-                error={Boolean(errors.endDate)}
-                helperText={touched.endDate && errors.endDate}
+            </FormControl>
+          </Grid>
+          <Grid item xs={6} lg={4}>
+            <FormControl required sx={{ width: "100%", height: 40 }}>
+              <DatePicker
+                renderInput={(props) => (
+                  <TextField
+                    name="endDate"
+                    size="small"
+                    variant="outlined"
+                    {...props}
+                    error={Boolean(errors.endDate)}
+                    helperText={touched.endDate && errors.endDate}
+                    sx={{ width: "100%" }}
+                  />
+                )}
+                label="Event end date"
+                value={values.endDate}
+                onChange={(newValue) => {
+                  if (!newValue) setFieldValue("endDate", null)
+                  setFieldValue(
+                    "endDate",
+                    moment(newValue, "DD/MM/YYYY HH:mm:ss").toDate()
+                  )
+                }}
               />
-            )}
-            label="Event end date"
-            value={values.endDate}
-            onChange={(newValue) => {
-              if (!newValue) setFieldValue("endDate", null)
-              setFieldValue(
-                "endDate",
-                moment(newValue, "DD/MM/YYYY HH:mm:ss").toDate()
-              )
-            }}
-          />
-        </FormControl>
+            </FormControl>
+          </Grid>
 
-        <FormControl sx={{ display: "flex", flex: 1 }}>
-          <TextFieldWrapper
-            name="searchTerm"
-            label="searchTerm"
-            value={values.searchTerm}
-            onChange={handleChange}
-            id="searchTerm-input"
-            data-testid="searchTerm-input"
-            placeholder="searchTerm"
-            variant="outlined"
-            size="small"
-            error={errors.searchTerm != null}
-            helperText={touched.searchTerm ? errors.searchTerm : undefined}
-          />
-        </FormControl>
-
-        <ContainedButton
-          type="submit"
-          label="Search"
-          variant="contained"
-          icon={<Search />}
-          onClick={handleSubmit}
-        />
+          <Grid item xs={6} lg={10}>
+            <Box sx={{ display: "flex" }}>
+              <FormControl sx={{ display: "flex", flex: 1 }}>
+                <TextFieldWrapper
+                  name="searchTerm"
+                  label="searchTerm"
+                  value={values.searchTerm}
+                  onChange={handleChange}
+                  id="searchTerm-input"
+                  data-testid="searchTerm-input"
+                  placeholder="searchTerm"
+                  variant="outlined"
+                  size="small"
+                  error={errors.searchTerm != null}
+                  helperText={
+                    touched.searchTerm ? errors.searchTerm : undefined
+                  }
+                />
+              </FormControl>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            lg={2}
+            sx={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <ContainedButton
+              type="submit"
+              label="Search"
+              variant="contained"
+              icon={<Search />}
+              width="120px"
+              onClick={handleSubmit}
+              sx={{ height: 40 }}
+            />
+          </Grid>
+        </Grid>
       </ContainerCard>
     </form>
   )
